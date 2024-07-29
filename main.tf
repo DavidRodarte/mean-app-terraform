8445b638c7c9511d0c7c6f594c41b60c9d9233c7
David Rodarte <drh697@gmail.com>
2024-07-28 19:56:10 -0600

    ec2 order

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ module "security_group" {
   source = "./modules/security-group"
 }
 
-module "ec2-app" {
-  subnet_id         = module.security_group.subnet_id
-  security_group_id = module.security_group.sg_http_id
-  source            = "./modules/ec2-app"
-}
-
 module "ec2-mongodb" {
   subnet_id         = module.security_group.subnet_id
   security_group_id = module.security_group.sg_mongo_id
   source            = "./modules/ec2-mongodb"
+}
+
+module "ec2-app" {
+  subnet_id         = module.security_group.subnet_id
+  security_group_id = module.security_group.sg_http_id
+  source            = "./modules/ec2-app"
 }
 
 module "load-balancer" {
